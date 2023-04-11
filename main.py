88f4940d6ab2560ca867434258c0915fa6ff68cd
vebnik <vebnik111@gmail.com>
2023-04-11 18:44:59 +0300

    some little fix

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ from src.tree_store import TreeStore
 def main() -> None:
     store = TreeStore(items)
 
-    # print(store.get_item(8))
-    # print(store.get_children(4))
-    # print('Search', store.get_all_parents(8))
+    print(store.get_item(8))
+    print(store.get_children(4))
+    print(store.get_all_parents(7))
     print(store.get_all())
-    print(items)
 
 
 if __name__ == '__main__':
